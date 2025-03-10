3b38ff0b1b9777afd245edb8a99e521beeb6faf7
LeThanhNhan <lnhan8132@gmail.com>
2025-03-10 22:14:50 +0700

    update Model

--- models/Model.py
+++ models/Model.py
@@ -1,4 +1,4 @@
-
+from keras.src.legacy.preprocessing.image import ImageDataGenerator
 from tensorflow.keras import layers, models
 # Xây dựng mô hình CNN
 def build_model(input_shape=(150, 150, 3)):
@@ -14,3 +14,23 @@ def build_model(input_shape=(150, 150, 3)):
     ])
     model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
     return model
+# Chuẩn bị dữ liệu huấn luyện và kiểm tra
+def prepare_data(train_dir, validation_dir, batch_size=32, img_size=(150, 150)):
+    train_datagen = ImageDataGenerator(rescale=1./255, horizontal_flip=True, rotation_range=40, shear_range=0.2)
+    test_datagen = ImageDataGenerator(rescale=1./255)
+
+    train_generator = train_datagen.flow_from_directory(
+        train_dir,
+        target_size=img_size,
+        batch_size=batch_size,
+        class_mode='binary'
+    )
+
+    validation_generator = test_datagen.flow_from_directory(
+        validation_dir,
+        target_size=img_size,
+        batch_size=batch_size,
+        class_mode='binary'
+    )
+
+    return train_generator, validation_generator
