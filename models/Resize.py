131c66f65fb5eaeece0c83b4c88166f7b52130f2
LeThanhNhan <lnhan8132@gmail.com>
2025-03-14 22:28:29 +0700

    Xử lý hình ảnh thu thập có kích thước chuẩn 224x224

---
+++ models/Resize.py
@@ -0,0 +1,15 @@
+from PL import Image
+import os
+
+def resize_image(input_dir,output_dir,size=(224,224)):
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    for filename in os.listdir(input_dir):
+        in filename.endswith('.jpg', '.jpeg', 'png');
+        try:
+            img = Image.open(os.path.join(input_dir, filename))
+            img_resized = img.resize(size)
+            img_resized.save(os.path.join(output_dir, filename))
+        except Exception as e:
+            print(f"Error processing {filename}: {e}")
+resize_image('Z:\HandleData','Z:\GarbageClassification\data\non_recyclable\trash')
