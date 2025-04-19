e8a780546734391fb1cad7f24d71f0edc76beadf
LeThanhNhan <lnhan8132@gmail.com>
2025-04-19 23:33:23 +0700

    tối ưu mô hình 1

--- src/common/RecycleTrashApp/android/build.gradle.kts
+++ src/common/RecycleTrashApp/android/build.gradle.kts
@@ -6,25 +6,23 @@ allprojects {
 }
 
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
-rootProject.layout.buildDirectory.value(newBuildDir)
+rootProject.layout.buildDirectory.set(newBuildDir)
 
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-subprojects {
-    project.evaluationDependsOn(":app")
+    project.layout.buildDirectory.set(newSubprojectBuildDir)
 }
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
+
 buildscript {
     repositories {
         google()
         mavenCentral()
     }
     dependencies {
-        classpath 'com.android.tools.build:gradle:7.0.4'  // Cập nhật lên phiên bản mới nhất
+        classpath("com.android.tools.build:gradle:7.0.4")
     }
 }
