9a50e0cacc2169d740cd5f20cbbd836b78aeb386
LeThanhNhan <lnhan8132@gmail.com>
2025-04-22 01:41:49 +0700

    handle dataset

--- models/RenameFile.py
+++ models/RenameFile.py
@@ -1,11 +1,11 @@
 import os
-paths = "Z:\\GarbageClassification\\data\\non_recyclable\\trash"
+paths = "Z:\\GarbageClassification\\data\\non_recyclable\\battery"
 
 start =1
 #Đổi tên file
 for i ,filename in enumerate(sorted(os.listdir(paths))):
     if filename.endswith(".jpg") or filename.endswith(".png"):
-        new_name =f"trash{start + i}.jpg"
+        new_name =f"battery{start + i}.jpg"
         old_path =os.path.join(paths,filename)
         new_path = os.path.join(paths,new_name)
         os.rename(old_path,new_path)
