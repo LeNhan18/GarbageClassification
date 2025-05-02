275a17554efde233d7e3dee59cb44e8bb283ec5a
LeThanhNhan <lnhan8132@gmail.com>
2025-05-02 11:02:13 +0700

    update model2a

--- models/TrainModel1.py
+++ models/TrainModel1.py
@@ -29,7 +29,7 @@ except Exception as e:
 data_dir = 'Z:\\GarbageClassification\\data'
 img_size = (240, 240)  # Tăng kích thước ảnh một chút
 batch_size = 16  # Giảm batch size để cải thiện độ chính xác
-epochs = 10  # Tăng số epochs hơn nữa (Adjust this based on your training time)
+epochs = 50  # Tăng số epochs hơn nữa (Adjust this based on your training time)
 input_shape = (240, 240, 3)
 
 # --- Data Augmentation mạnh hơn ---
