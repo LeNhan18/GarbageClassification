b10131081b266d28ea922ce7430e7488e1c43656
LeThanhNhan <lnhan8132@gmail.com>
2025-05-06 21:07:52 +0700

    update app

--- src/common/RecycleTrashApp/android/settings.gradle.kts
+++ src/common/RecycleTrashApp/android/settings.gradle.kts
@@ -22,4 +22,14 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.8.22" apply false
 }
 
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "android"
 include(":app")
+include(":image_gallery_saver")
